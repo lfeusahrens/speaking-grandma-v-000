8bc92d36b4d69536233b37efcaf3bf380f82a578
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-23 21:40:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,9 @@ def speak_to_grandma(phrase)
   if phrase.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
-    
+    "NO, NOT SINCE 1938!"
   else phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
-    
   end
 end
 
